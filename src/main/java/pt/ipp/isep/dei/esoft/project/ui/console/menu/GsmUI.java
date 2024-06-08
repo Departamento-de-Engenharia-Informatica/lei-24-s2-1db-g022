11ac22c5c2c9d7633acd03b3077992ae2cd9f2a5
1191330 <1191330@isep.ipp.pt>
2024-06-08 16:52:50 +0100

    Menu Update
    Co-Authored-By: Daniel <1170499@isep.ipp.pt>
    Co-Authored-By: Tomás Pereira <1191377@isep.ipp.pt>

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GsmUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GsmUI.java
@@ -32,6 +32,11 @@ public class GsmUI implements Runnable {
         options.add(new MenuItem("US13 - Apply Algorithm.", new MinSpanTreeUI()));
         options.add(new MenuItem("US17 - Place sign to assembly point.", new MinRouteToApUI()));
         options.add(new MenuItem("US18 - Place sign to evacuate to one of the several Assembly Points.", new ShortestPathApUI()));
+        options.add(new MenuItem("US20 - Register GreenSpace.", new ShortestPathApUI()));
+        options.add(new MenuItem("US21 - Add New Entry ToDoList.", new ShortestPathApUI()));
+        options.add(new MenuItem("US22 - Add New Entry Agenda.", new ShortestPathApUI()));
+        options.add(new MenuItem("US23 - Assign Team To Entry Agenda.", new ShortestPathApUI()));
+        options.add(new MenuItem("US27 - List GreenSpaces Managed by me.", new ShortestPathApUI()));
 
         int option = 0;
         do {
